2b4a17ac52a28f6218ee4b8dc524698ca9d4cb51
Liuyubao <523331232@qq.com>
2019-04-24 21:53:58 -0400

    change all the bg and complete MyMsgVC

---
+++ URBuddy/MyMsgVC.swift
@@ -0,0 +1,30 @@
+//
+//  MyMsgVC.swift
+//  URBuddy
+//
+//  Created by Liuyubao on 4/24/19.
+//  Copyright © 2019 xinghaiwulian. All rights reserved.
+//
+
+import UIKit
+
+class MyMsgVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
